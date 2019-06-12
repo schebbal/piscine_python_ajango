3282c81437d9502b4bb90a2ed1b3350b55766a30
Said Chebbal <schebbal@z3r6p1.le-101.fr>
2019-06-12 18:24:45 +0200

    02

--- D01/ex07/periodic_table.py
+++ D01/ex07/periodic_table.py
@@ -11,41 +11,76 @@
 #                                                         /                    #
 # **************************************************************************** #
 
-def periodic_table():
-    d=[
-        ('Hendrix'   , '1942'),
-        ('Allman'    , '1946'),
-        ('King'      , '1925'),
-        ('Clapton'   , '1945'),
-        ('Johnson'   , '1911'),
-        ('Berry'     , '1926'),
-        ('Vaughan'   , '1954'),
-        ('Cooder'    , '1947'),
-        ('Page'      , '1944'),
-        ('Richards'  , '1943'),
-        ('Hammett'   , '1962'),
-        ('Cobain'    , '1967'),
-        ('Garcia'    , '1942'),
-        ('Beck'      , '1944'),
-        ('Santana'   , '1947'),
-        ('Ramone'    , '1948'),
-        ('White'     , '1975'),
-        ('Frusciante', '1970'),
-        ('Thompson'  , '1949'),
-        ('Burton'    , '1939')
-    ]
+def string_to_dict(line):
+    my_dict = dict()
+    elem_split = line.split("=")
+    #print(elem_split)
+    my_dict['name'] = elem_split[0].strip(" ")
+    posi_split = elem_split[1].split(",")
+    #print(posi_split)
     
-    mon_dict = dict() # je cree un dict vide
-    for couple in d: # je parcours la liste d
-        if mon_dict.get(couple[1]): # s'il y a deja une valeur pour cette cle, j'ajoute a la liste de valeurs
-            mon_dict[couple[1]].append(couple[0])
-        else: # s'il n'y a pas de valeur pour cette cle, j'insere une liste
-            mon_dict[couple[1]] = [couple[0]]
+    for data in posi_split:
+        data_split = data.split(":")
+        #print(third_split)
+        my_dict[data_split[0].strip(" ")] = data_split[1].strip(" \n")
+    return my_dict
 
-    for cle in sorted(mon_dict): # je parcours le dictionnire
-        for v in sorted(mon_dict[cle]): # je transforme la liste en string
-            #print("%s : %s" % (cle, v))
-            print("%s" % v)
+def read_file():
+    """ renvoie un tableau rempli a partir du fichier """
+    with open("periodic_table.txt") as f:
+        my_tab = []
+        for line in f:
+            my_dict = string_to_dict(line)
+            my_tab.append(my_dict)
+            #print("------- : %s", my_tab, "\n")
+        return my_tab
 
-if __name__ == '__main__':
-    periodic_table()
+def header(f):
+	f.write("<!DOCTYPE html>\n")
+	f.write("<html lang='en'>\n")
+	f.write("  <head>\n    <meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'>\n    <title>Periodic table</title>\n")
+	f.write("    <style>table {	width: 100%;} td {border: 1px solid black; padding:10px} </style>\n  </head>\n")
+
+def footer(f):
+	f.write("</html>\n")
+
+def content(f, tab):
+	previous = 0
+	current = 0
+	s = "    <table>\n"
+	for elem in tab:
+		current = int(elem['position'])
+		if current == 0:
+			s +="      <tr>\n"
+		if current - previous > 1:
+			s += "        <td colspan='" + str(current - previous - 1) + "'></td>\n"
+        s += "        <td><h4>" + elem['name'] + "</h4>\n"
+        s += "          <ul><li>" + elem['number'] +"</li><li>" + elem['small'] +"</li><li>" + elem['molar'] + "</li></ul></td>\n"
+        if current == 17:
+            s += "      </tr>\n"
+            current = 0
+        previous = current
+
+	s += "    </table>\n"	
+	f.write(s)
+
+def body(f, tab):
+	f.write("  <body>\n")
+	content(f, tab)
+	f.write("  </body>\n")
+
+def write_to_file(tab):
+	#print(tab)
+	with open("periodic_table.html", "w") as f: 
+		header(f)
+		body(f, tab)
+		footer(f)
+
+def generate_html():
+    my_tab = read_file()
+    #for elem in my_tab:
+    #    print(elem)
+    write_to_file(my_tab)
+    
+if __name__ == '__main__' :
+	generate_html()
